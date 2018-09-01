801e9338aa22f6117dd581a678c4483ad2a549df
Aaron <aaronmeade9292@gmail.com>
2018-09-01 10:04:43 -0700

    added logger

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,5 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer/categoryReducer.js';
 import formValidate from './middleware/formValidate.js';
+import logger from './middleware/logger.js';
 
-export default createStore(reducer, applyMiddleware(formValidate), window._REDUX_DEVTOOLS_EXTENSION_&& window._REDUX_DEVTOOLS_EXTENSION_())
+export default createStore(reducer, applyMiddleware(formValidate, logger), window._REDUX_DEVTOOLS_EXTENSION_&& window._REDUX_DEVTOOLS_EXTENSION_())
